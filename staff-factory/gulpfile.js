9ff981abc2b2aab52be6c6344d003fb6784b4cc9
DmitryMorar <dmitr.morar@gmail.com>
2017-01-08 16:34:29 +0200

    Main files of project

---
+++ staff-factory/gulpfile.js
@@ -0,0 +1,35 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    browserSync = require('browser-sync'),
+    sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('sass', function () {
+    return gulp.src('app/scss/main.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('app/css'))
+        .pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('browser-sync', function () {
+    browserSync.init({
+        proxy: "cv/staff-factory/app/"
+    });
+});
+
+
+// gulp.task('browser-sync', function() {
+//     browserSync({
+//         server: {
+// 			baseDir: 'app/'
+//         },
+//         notify: false
+//     });
+// });
+
+gulp.task('watch', ['browser-sync', 'sass'], function () {
+    gulp.watch('app/scss/**/*.scss', ['sass']);
+    gulp.watch('app/js/*.js', browserSync.reload);
+    gulp.watch('app/*.html', browserSync.reload);
+});
